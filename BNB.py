9888eb8b976d6d743b08be07ff172c1e9fb00c5e
xzhang964 <93890009+xzhang964@users.noreply.github.com>
2021-11-19 17:53:54 -0500

    Add files via upload

---
+++ BNB.py
@@ -0,0 +1,147 @@
+import argparse
+from typing import Dict
+import math
+import numpy as np
+import os
+import sys
+import time
+import networkx as nx
+from BranchState import BranchState
+# from graph import Graph
+
+class BranchAndBound:
+    def __init__(self, graph, limit=600):
+        self.graph = graph
+        self.winner = None
+        self.results = []
+        self.limit = limit
+
+    def run_DFS(self, graph, matrix):
+        stack = []
+        initial_city = 1
+        a = graph.copy()
+        initial_state = BranchState(graph.copy(), [], 0, matrix)
+        initial_state.add_stop(initial_city)
+        stack.append(initial_state)
+        i = 0
+
+        while len(stack):
+            if float(time.time()) - float(self.begin_time) > float(self.limit):
+                break
+
+            last_state = stack.pop()
+            if not self.winner or last_state.bound_val < self.winner.bound_val:
+
+                # checking for a candidate solution
+                if len(graph.nodes.keys()) == len(last_state.path):
+
+                    # checking if we have a cycle
+                    if last_state.path[0] in graph[last_state.path[-1]].keys():
+                        last_state.add_stop(last_state.path[0])
+
+                        # checking if upper bound needs to be updated
+                        if not self.winner or self.winner.bound_val > last_state.bound_val:
+                            i += 1
+                            self.results.append(
+                                (last_state.path, last_state.path_cost, time.time() - self.begin_time))
+                            print("Solution" + str(i) + ": ",
+                                  (last_state.path, last_state.path_cost, time.time() - self.begin_time))
+                            self.winner = last_state
+                else:
+                    sorted_list = self.sort_edges(graph[last_state.path[-1]])
+                    for node, cost in sorted_list:
+                        if node not in last_state.path:
+                            new_state = BranchState(graph, last_state.path[:], last_state.path_cost,
+                                                                matrix)
+                            new_state.add_stop(node)
+
+                            # checking if a branch can be pruned
+                            if not self.winner or self.winner.bound_val > new_state.bound_val:
+                                stack.append(new_state)
+
+    def sort_edges(self, edge_dict):
+        tup = []
+        for key in edge_dict:
+            tup.append((key, edge_dict[key]['weight']))
+
+        sorted(tup, key=lambda edge: edge[1], reverse=True)
+
+        return tup
+
+    def generate_tour(self):
+        graph = self.graph
+        mat = [[0 for i in range(len(graph.nodes.keys()))] for j in range(len(graph.nodes.keys()))]
+        for i in range(len(graph.nodes.keys())):
+            for j in range(len(graph.nodes.keys())):
+                if i != j:
+                    mat[i][j] = graph[i + 1][j + 1]['weight']
+                else:
+                    mat[i][j] = sys.maxsize
+        self.mat = np.array(mat)
+        self.begin_time = time.time()
+        self.run_DFS(graph, self.mat)
+
+        return self.results
+
+if __name__ == '__main__':
+    # process arguments
+    start = time.time()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-inf', action='store', dest='inf')
+    parser.add_argument('-time', action='store', dest='time', default=1)
+    parser.add_argument('-seed', action='store', dest='seed', default=0)
+    args, unknown = parser.parse_known_args()
+    # load graph
+    # graph = Graph(args.inf)
+    city_dict = {}
+    with open(args.inf) as f:
+        while True:
+            line = f.readline()
+            if 'NODE_COORD_SECTION\n' in line:
+                break
+        for line in f:
+            if 'EOF\n' in line:
+                break
+            v = line.split(' ')
+            city_dict[int(v[0])] = {'x': float(v[1]), 'y': float(v[2])}
+
+    # building cost matrix for each edge
+    graph = nx.Graph()
+    for u in city_dict:
+        for v in city_dict:
+            if u != v:
+                x_dist = city_dict[u]['x'] - city_dict[v]['x']
+                y_dist = city_dict[u]['y'] - city_dict[v]['y']
+                val = int(round(math.sqrt(x_dist ** 2 + y_dist ** 2)))
+                graph.add_node(u)
+                graph.add_node(v)
+                graph.add_edge(u, v, weight=val)
+    print(graph.number_of_nodes(), "nnnnnn")
+    np.random.seed(int(args.seed))
+    # test with relative path
+    trace_file = '{}_{}_{}_{}.trace'.format('output/' + args.inf.split('/')[-1][:-4], 'BNB',
+                                            args.time, args.seed)
+    if os.path.isfile(trace_file):
+        os.remove(trace_file)
+
+    # set the initial tour from 0 to 0
+    # tour = list(np.random.permutation(list(range(0, graph.n))))
+    # # tour = list(range(0, graph.n))
+    # tour = tour + [tour[0]]
+    bnb = BranchAndBound(graph, args.time)
+    tour = bnb.generate_tour()
+
+    # output solution file
+    solution_file = '{}_{}_{}_{}.sol'.format('output/' + args.inf.split('/')[-1][:-4], 'BNB',
+                                             args.time, args.seed)
+    with open(solution_file, 'w') as f:
+        f.write(str(distance))
+        f.write('\n')
+        f.write(','.join(map(str, tour)))
+
+    trace_file = '{}_{}_{}_{}.trace'.format('output/' + args.inf.split('/')[-1][:-4], 'BNB',
+                                            args.time, args.seed)
+    delta = time.time() - start
+    with open(trace_file, 'a') as f:
+        f.write('{:.2f}, {}\n'.format(delta, distance))
+
---
+++ BranchState.py
@@ -0,0 +1,63 @@
+import networkx as nx
+import numpy as np
+import sys
+
+class BranchState:
+
+	def __init__(self,graph, path, path_cost, matrix):
+		self.graph = graph
+		self.path = path
+		self.path_cost = path_cost
+		self.matrix = matrix
+
+
+	def add_stop(self, stop):
+		if len(self.path):
+			self.path_cost = self.path_cost + self.graph[self.path[-1]][stop]['weight']
+		self.path.append(stop)
+		# self.bound_val = self.get_1tree_lower_bound(self.graph, self.path, self.path_cost)
+		self.bound_val = self.get_min_dist_lower_bound(self.matrix, self.path, self.path_cost)
+
+
+	def get_1tree_lower_bound(self, graph, path, path_cost):
+		sub_graph = graph.copy()
+		if len(path) > 1 and path[0] == path[-1]:
+			path = path[:-1]
+
+		for n in path:
+			sub_graph.remove_node(n)
+
+		mst = nx.minimum_spanning_tree(sub_graph)
+		start_u = path[0]
+		start_v = self.get_cheapest_neighbour(start_u,graph)
+		end_u = path[-1]
+		end_v = self.get_cheapest_neighbour(end_u,graph)
+
+		mst.add_edge(start_u, start_v, weight=graph[start_u][start_v]['weight'])
+		mst.add_edge(end_u, end_v, weight= graph[end_u][end_v]['weight'])
+
+		return path_cost + mst.size(weight='weight')
+
+
+	def get_min_dist_lower_bound(self, matrix, path, path_cost):
+		mat = matrix.copy()
+		if len(path) > 1:
+			i=0
+			while i < len(path)-2:
+				mat[path[i]-1] = sys.maxsize
+				mat[path[i+1]-1] = sys.maxsize
+				i += 2
+		row_min = np.amin(mat, axis=1)
+		mat = mat - np.reshape(row_min,(len(matrix),1))
+		col_min = np.amin(mat, axis=0)
+
+		return np.sum(row_min) + np.sum(col_min) + path_cost
+
+
+	def get_cheapest_neighbour(self, node, graph):
+		temp_dict = graph[node]
+		tup = []
+		for key in temp_dict:
+			tup.append((key,temp_dict[key]['weight']))
+
+		return min(tup,key =lambda edge:edge[1])[0]
