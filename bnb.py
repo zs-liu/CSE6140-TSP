a3886ada0833f4406b36fbe833d06b999705b3d5
zs-liu <zsliu98@gmail.com>
2021-11-29 20:38:31 -0500

    fix bugs of bnb

--- bnb.py
+++ bnb.py
@@ -5,33 +5,33 @@ import os
 import sys
 import time
 import networkx as nx
-from BranchState import BranchState
+from branch_state import BranchState
+from graph import Graph
+
 
 class BranchAndBound:
-    def __init__(self, graph, limit=600):
-        self.graph = graph
+    def __init__(self, g, start, solution_file, trace_file):
+        self.g = g
         self.winner = None
-        self.results = []
-        self.limit = limit
+        self.start = start
+        self.solution_file = solution_file
+        self.trace_file = trace_file
 
-    def run_DFS(self, graph, matrix):
+    def run_DFS(self, matrix):
         stack = []
         initial_city = 1
-        a = graph.copy()
-        initial_state = BranchState(graph.copy(), [], 0, matrix)
+        initial_state = BranchState(self.g.copy(), [], 0, matrix)
         initial_state.add_stop(initial_city)
         stack.append(initial_state)
         i = 0
 
         while len(stack):
-            if float(time.time()) - float(self.begin_time) > float(self.limit):
-                break
 
             last_state = stack.pop()
             if not self.winner or last_state.bound_val < self.winner.bound_val:
 
                 # checking for a candidate solution
-                if len(graph.nodes.keys()) == len(last_state.path):
+                if self.g.number_of_nodes() == len(last_state.path):
 
                     # checking if we have a cycle
                     if last_state.path[0] in graph[last_state.path[-1]].keys():
@@ -39,19 +39,20 @@ class BranchAndBound:
 
                         # checking if upper bound needs to be updated
                         if not self.winner or self.winner.bound_val > last_state.bound_val:
-                            i += 1
-                            last_state.path = [ele-1 for ele in last_state.path]
-                            self.results.append(
-                                (last_state.path, time.time() - self.begin_time, last_state.path_cost ))
-                            print("Solution" + str(i) + ": ",
-                                  (last_state.path, time.time() - self.begin_time, last_state.path_cost))
+                            with open(self.solution_file, 'w+') as f:
+                                f.write(str(last_state.path_cost))
+                                f.write('\n')
+                                f.write(','.join(map(str, last_state.path)))
+                            delta = time.time() - self.start
+                            with open(self.trace_file, 'a') as f:
+                                f.write('{:.2f}, {}\n'.format(delta, last_state.path_cost))
                             self.winner = last_state
                 else:
-                    sorted_list = self.sort_edges(graph[last_state.path[-1]])
+                    sorted_list = self.sort_edges(self.g[last_state.path[-1]])
                     for node, cost in sorted_list:
                         if node not in last_state.path:
-                            new_state = BranchState(graph, last_state.path[:], last_state.path_cost,
-                                                                matrix)
+                            new_state = BranchState(self.g.copy(), last_state.path[:], last_state.path_cost,
+                                                    matrix)
                             new_state.add_stop(node)
 
                             # checking if a branch can be pruned
@@ -68,19 +69,15 @@ class BranchAndBound:
         return tup
 
     def generate_tour(self):
-        graph = self.graph
-        mat = [[0 for i in range(len(graph.nodes.keys()))] for j in range(len(graph.nodes.keys()))]
-        for i in range(len(graph.nodes.keys())):
-            for j in range(len(graph.nodes.keys())):
+        mat = np.zeros((self.g.number_of_nodes(), self.g.number_of_nodes()))
+        for i in range(self.g.number_of_nodes()):
+            for j in range(self.g.number_of_nodes()):
                 if i != j:
-                    mat[i][j] = graph[i + 1][j + 1]['weight']
+                    mat[i][j] = graph[i][j]['weight']
                 else:
                     mat[i][j] = sys.maxsize
-        self.mat = np.array(mat)
-        self.begin_time = time.time()
-        self.run_DFS(graph, self.mat)
+        self.run_DFS(np.array(mat))
 
-        return self.results
 
 if __name__ == '__main__':
     # process arguments
@@ -91,59 +88,11 @@ if __name__ == '__main__':
 
     args, unknown = parser.parse_known_args()
 
+    graph = Graph(args.inf)
+    graph = nx.from_numpy_matrix(graph.distance)
 
-    city_dict = {}
-    with open(args.inf) as f:
-        while True:
-            line = f.readline()
-            if 'NODE_COORD_SECTION\n' in line:
-                break
-        for line in f:
-            if 'EOF\n' in line:
-                break
-            v = line.split(' ')
-            city_dict[int(v[0])] = {'x': float(v[1]), 'y': float(v[2])}
+    solution_file = '{}_{}_{}.sol'.format('output/' + args.inf.split('/')[-1][:-4], 'BnB', args.time)
+    trace_file = '{}_{}_{}.trace'.format('output/' + args.inf.split('/')[-1][:-4], 'BnB', args.time)
 
-    # building cost matrix for each edge
-    graph = nx.Graph()
-    for u in city_dict:
-        for v in city_dict:
-            if u != v:
-                x_dist = city_dict[u]['x'] - city_dict[v]['x']
-                y_dist = city_dict[u]['y'] - city_dict[v]['y']
-                val = int(round(math.sqrt(x_dist ** 2 + y_dist ** 2)))
-                graph.add_node(u)
-                graph.add_node(v)
-                graph.add_edge(u, v, weight=val)
-
-    # test with relative path
-    trace_file = '{}_{}_{}.trace'.format('output/' + args.inf.split('/')[-1][:-4], 'bnb',
-                                         args.time)
-    if os.path.isfile(trace_file):
-        os.remove(trace_file)
-
-    bnb = BranchAndBound(graph, args.time)
-    tour = bnb.generate_tour()
-
-    trace_list = []
-    for line in tour:
-        round2 = round(line[1], 2)
-        trace_list.append([round2, line[2]])
-
-    def process_dormat(trace_one):
-        return "{:.2f},{:d}".format(trace_one[0], trace_one[1])
-
-    for i in range(len(tour)):
-        # output trace file
-        trace_file = '{}_{}_{}_{}.trace'.format('output/' + args.inf.split('/')[-1][:-4], 'bnb',
-                                             args.time, i)
-        with open(trace_file, 'w') as f:
-            f.write('\n'.join(map(str, trace_list[i])))
-
-        # output solution file
-        solution_file = '{}_{}_{}_{}.sol'.format('output/' + args.inf.split('/')[-1][:-4], 'bnb',
-                                              args.time, i)
-        with open(solution_file, 'w+') as f:
-            distance = tour[i][2]
-            f.write('{}\n'.format(distance))
-            f.write(','.join(map(str,tour[i][0])))
+    bnb = BranchAndBound(graph, start=start, solution_file=solution_file, trace_file=trace_file)
+    bnb.generate_tour()
